56f43073125f52c3bfc8dc9f1742b8c105807243
prats4344 <kanungopratiyusha@gmail.com>
2025-07-13 18:55:53 +0530

    Update app.py for Render deployment

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ if __name__ == '__main__':
     if not os.path.exists('data.db'):
         with app.app_context():
             db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
 
 
 
